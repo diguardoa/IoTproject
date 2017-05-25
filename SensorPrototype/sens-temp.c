874e6dd31ec8a1a09662ae4539293bddadb77ed1
diguardoa <diguardoa@gmail.com>
2017-05-25 18:46:29 +0200

    managing temperature changes

--- SensorPrototype/sens-temp.c
+++ SensorPrototype/sens-temp.c
@@ -5,16 +5,27 @@
 #include "contiki-net.h"
 #include "rest-engine.h"
 
-int value = 0;
-//int end_value = 30;
+
+#define TIME_SAMPLING 100
+#define STARTING_TEMPERATURE 20
+#define A 0.9048
+// A = exp(-(TIME_SAMPLING/TIME_CONST)
+#define B (1 - A)
+
+// First order variable
+static int u_k_1 = (STARTING_TEMPERATURE+1);
+static int u_k = (STARTING_TEMPERATURE+1);
+static float temp_k = STARTING_TEMPERATURE;	// temperature at current time
+static float temp_k_1 = STARTING_TEMPERATURE;	// temperature last sample
+ 
 
 void temp_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 void temp_post_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void temp_periodic_handler();
 
 
-PERIODIC_RESOURCE(temp_sens,"title=\"Temperature Sensor\"; rt=\"Temperature\"", temp_get_handler,temp_post_handler,NULL,NULL,10*CLOCK_SECOND
-	,temp_periodic_handler);
+PERIODIC_RESOURCE(temp_sens,"title=\"Temperature Sensor\"; rt=\"Temperature\"", temp_get_handler,temp_post_handler,NULL,NULL,
+	TIME_SAMPLING,temp_periodic_handler);
 
 void temp_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
@@ -22,7 +33,7 @@ void temp_get_handler(void* request, void* response, uint8_t *buffer, uint16_t p
 	char message[20];
 	int length = 20;
 
-	sprintf(message, "VALUE:%03u", value);
+	sprintf(message, "TEMPERATURE:%03d", ((int) temp_k));
 	length = strlen(message);
 	memcpy(buffer, message, length);
 
@@ -40,7 +51,11 @@ void temp_post_handler(void* request, void* response, uint8_t *buffer, uint16_t 
      
   if( len > 0 ){
      new_value = atoi(val);	
-     value = new_value;
+     u_k = new_value + 1; 
+     /* 
+     * because it doesn't reach the final value. It's done only for the coherence of the
+     * simulation
+     */
      REST.set_response_status(response, REST.status.CREATED);
   } else {
      REST.set_response_status(response, REST.status.BAD_REQUEST);
@@ -49,9 +64,11 @@ void temp_post_handler(void* request, void* response, uint8_t *buffer, uint16_t 
 
 static void temp_periodic_handler()
 {
-	value++;
-
-	REST.notify_subscribers(&temp_sens);
+	temp_k_1 = temp_k;
+	temp_k = (A * temp_k_1) + (B * u_k_1);
+	if (((int) temp_k) != ((int) temp_k_1))
+		REST.notify_subscribers(&temp_sens);
+	u_k_1 = u_k;
 }
 
 PROCESS(temperature_process, "Temperature sensor");
@@ -61,6 +78,8 @@ AUTOSTART_PROCESSES(&temperature_process);
 PROCESS_THREAD(temperature_process, ev, data)
 {
 	PROCESS_BEGIN();
+
+
 
 	/* Initialize Rest engine */
 	rest_init_engine();
