b4fed3752d7994cb9b27033b9867a7058b966648
diguardoa <diguardoa@gmail.com>
2017-05-26 13:42:18 +0200

    fixed oM2M

--- oM2M/start.sh
+++ oM2M/start.sh
@@ -1,2 +1,3 @@
-sudo java -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true IN/plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit
+gnome-terminal -x sudo java -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true IN/plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit
+gnome-terminal -x sudo java -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true MN/plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit
 
