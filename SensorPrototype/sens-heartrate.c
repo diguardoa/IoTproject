d602e91df6936c91d12e29aa6f03539d98d4b784
AndreV2890 <andrea.vincentini28@gmail.com>
2017-05-31 14:56:11 +0200

    Update Sensor Prototype

--- SensorPrototype/sens-heartrate.c
+++ SensorPrototype/sens-heartrate.c
@@ -6,13 +6,49 @@
 #include "rest-engine.h"
 
 int value = 0;
+int pat_id = 0;
 
+void id_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+void id_post_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 void hr_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 void hr_post_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void hr_periodic_handler();
 
-PERIODIC_RESOURCE(hr_sens,"title=\"HeartRate Sensor\"; rt=\"Rate\"", hr_get_handler,hr_post_handler,NULL,NULL,10*CLOCK_SECOND
+PERIODIC_RESOURCE(hr_sens,"title=\"HRS\";rt=\"Rate\";type=\"S\";obs", hr_get_handler,hr_post_handler,NULL,NULL,10*CLOCK_SECOND
 	,hr_periodic_handler);
+
+RESOURCE(PatId, "title=\"PatienId\";rt=\"Id\"", id_get_handler, id_post_handler, NULL, NULL);
+
+void id_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	/* Populat the buffer with the response payload*/
+	char message[20];
+	int length = 20;
+
+	sprintf(message, "pat_id:%03u", pat_id);
+	length = strlen(message);
+	memcpy(buffer, message, length);
+
+	REST.set_header_content_type(response, REST.type.TEXT_PLAIN); 
+	REST.set_header_etag(response, (uint8_t *) &length, 1);
+	REST.set_response_payload(response, buffer, length);
+}
+
+void id_post_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+  int new_id, len;
+  const char *val = NULL;
+     
+  len=REST.get_post_variable(request, "value", &val);
+     
+  if( len > 0 ){
+     new_id = atoi(val);	
+     pat_id = new_id;
+     REST.set_response_status(response, REST.status.CREATED);
+  } else {
+     REST.set_response_status(response, REST.status.BAD_REQUEST);
+  }
+}
 
 void hr_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
@@ -65,6 +101,7 @@ PROCESS_THREAD(heartRate_process, ev, data)
 
 	/* Activate the application-specific resources */
 	rest_activate_resource(&hr_sens, "heart_rate");
+	rest_activate_resource(&PatId, "pat_id");
 
 	while(1) {
 		PROCESS_WAIT_EVENT();
--- SensorPrototype/sens-oxygen.c
+++ SensorPrototype/sens-oxygen.c
@@ -6,12 +6,49 @@
 #include "rest-engine.h"
 
 int oxy_perc = 98;
+int pat_id = 0;
 
+void id_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+void id_post_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 void oxy_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 void oxy_post_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
 static void oxy_periodic_handler();
 
-PERIODIC_RESOURCE(oxy_sens, "title=\"Oxygen Sensor\"; rt=\"Percentage\"", oxy_get_handler, oxy_post_handler, NULL, NULL, 5*CLOCK_SECOND, oxy_periodic_handler);
+PERIODIC_RESOURCE(oxy_sens, "title=\"OS\";rt=\"Perc\";type=\"S\";obs", oxy_get_handler, oxy_post_handler, NULL, NULL, 5*CLOCK_SECOND, oxy_periodic_handler);
+
+RESOURCE(PatId, "title=\"PatienId\";rt=\"Id\"", id_get_handler, id_post_handler, NULL, NULL);
+
+void id_get_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+	/* Populat the buffer with the response payload*/
+	char message[20];
+	int length = 20;
+
+	sprintf(message, "pat_id:%03u", pat_id);
+	length = strlen(message);
+	memcpy(buffer, message, length);
+
+	REST.set_header_content_type(response, REST.type.TEXT_PLAIN); 
+	REST.set_header_etag(response, (uint8_t *) &length, 1);
+	REST.set_response_payload(response, buffer, length);
+}
+
+void id_post_handler(void* request, void* response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+{
+  int new_id, len;
+  const char *val = NULL;
+     
+  len=REST.get_post_variable(request, "value", &val);
+     
+  if( len > 0 ){
+     new_id = atoi(val);	
+     pat_id = new_id;
+     REST.set_response_status(response, REST.status.CREATED);
+  } else {
+     REST.set_response_status(response, REST.status.BAD_REQUEST);
+  }
+}
+
 
 void oxy_get_handler(void* request, void* response, uint8_t* buffer, uint16_t preferred_size, int32_t* offset)
 {
@@ -63,6 +100,7 @@ PROCESS_THREAD(oxygen_process, ev, data)
 
 	/* Activate the application-specific resources */
 	rest_activate_resource(&oxy_sens, "oxygen");
+	rest_activate_resource(&PatId, "pat_id");
 
 	while(1) {
 		PROCESS_WAIT_EVENT();
